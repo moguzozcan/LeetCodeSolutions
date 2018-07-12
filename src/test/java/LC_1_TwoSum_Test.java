964f9dd5f773f8e90fdc167a35982bf4ec40387d
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2018-07-12 16:32:49 +0300

    LC 1 TwoSum solution and test are added

---
+++ src/test/java/LC_1_TwoSum_Test.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LC_1_TwoSum_Test {
+
+    private LC_1_TwoSum lc_1_twoSum;
+
+    @Before
+    public void init() {
+        lc_1_twoSum = new LC_1_TwoSum();
+    }
+
+    @Test
+    public void testRegularly() {
+        int[] nums = new int[] {2, 7, 11, 15};
+        int target = 9;
+
+        int[] resArr = lc_1_twoSum.twoSum(nums, target);
+
+        Assert.assertEquals(resArr[0], 1);
+        Assert.assertEquals(resArr[1], 0);
+    }
+}
