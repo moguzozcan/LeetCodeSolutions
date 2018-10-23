e9f98ff24174bd2812996ed35404c03af03f4669
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2018-10-23 21:59:44 +0300

    PowerOfThree solution added

---
+++ src/main/java/LC_326_PowerOfThree.java
@@ -0,0 +1,41 @@
+/*
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+Example 3:
+
+Input: 9
+Output: true
+Example 4:
+
+Input: 45
+Output: false
+Follow up:
+Could you do it without using any loop / recursion?
+ */
+
+public class LC_326_PowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0) {
+            return false;
+        }
+
+        int rem;
+        while(n != 1) {
+            rem = n % 3;
+            if(rem != 0) {
+                return false;
+            }
+            n = n / 3;
+        }
+
+        return true;
+    }
+}
