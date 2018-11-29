e5286d2e7de7089d634afbf6e496d0ad3cda523e
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2018-11-29 09:30:17 +0300

    LC_448_FindAllNumbersDissappearedInAnArray added

---
+++ src/main/java/LC_448_FindAllNumbersDissappearedInAnArray.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+/*
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+ */
+public class LC_448_FindAllNumbersDissappearedInAnArray {
+
+    /*
+     The trick is that, loop through the array, for the elements, go to the location whose index is the one minus element,
+     then convert that element in that location to its negative one, if it is positive, meaning if it did not converted
+     before. Then, loop array again and find the ones that are positive, they are the ones that are not in the list,
+     but be careful that the index was one minus, so when finding the number add 1 to the indexes.
+     */
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for(int i : nums) {
+            if(nums[Math.abs(i) - 1] > 0) {
+                nums[Math.abs(i) - 1] = -1 * nums[Math.abs(i) - 1];
+            }
+        }
+
+        List<Integer> returnList = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                returnList.add(i + 1);
+            }
+        }
+        return returnList;
+    }
+}
