e46d02778c2669dd12bb16b61675985004fbf761
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-06-24 15:12:39 +0300

    LC 50 solution added

--- src/main/java/medium/LC_50_PowXN.java
+++ src/main/java/medium/LC_50_PowXN.java
@@ -1,6 +1,9 @@
 package medium;
 
 /*
+Difficulty: Medium
+Companies: Facebook
+
 Implement pow(x, n), which calculates x raised to the power n (xn).
 
 Example 1:
@@ -23,7 +26,48 @@ n is a 32-bit signed integer, within the range [−231, 231 − 1]
  */
 
 public class LC_50_PowXN {
+    /**
+     This is the iterative very basic solution, just need to check minus
+     and zero conditions.
 
+     Ranges could be important, maybe big integer? Or long
 
+     */
+    public double myPow(double x, int n) {
+        double result = 1;
+        if(n == 0) {
+            return 1.0;
+        } else {
+            if(n < 0) {
+                n = -n;
+                x = 1 / x;
+            }
+            if(n % 2 == 1) {
+                result = x;
+                n--;
+            }
+            while(n != 0) {
+                result = result * x * x;
+                n = n / 2;
+            }
+        }
 
+        return result;
+    }
+
+    /**
+     * This is the recursive solution just divides n by 2 when calling the function again
+     * @param x
+     * @param n
+     * @return
+     */
+    public double myPow2(double x, int n) {
+        if(n == 0)
+            return 1;
+        if(n<0){
+            n = -n;
+            x = 1/x;
+        }
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+    }
 }
