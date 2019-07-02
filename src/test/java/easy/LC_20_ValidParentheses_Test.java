644e4c05aab1cb08d6f1e890512a3811e9074b2f
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-02 16:59:28 +0300

    LC 20 tests added

--- src/test/java/easy/LC_20_ValidParentheses_Test.java
+++ src/test/java/easy/LC_20_ValidParentheses_Test.java
@@ -1,4 +1,36 @@
 package easy;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class LC_20_ValidParentheses_Test {
+
+    private LC_20_ValidParentheses lc_20_validParentheses;
+
+    @Before
+    public void init() {
+        lc_20_validParentheses  = new LC_20_ValidParentheses();
+    }
+
+    @Test
+    public void testParenthesisCorrect() {
+        String str = "()(){()}";
+
+        Assert.assertEquals(lc_20_validParentheses.isValid2(str), true);
+    }
+
+    @Test
+    public void testParenthesisFalse() {
+        String str = "()(){()}}";
+
+        Assert.assertEquals(lc_20_validParentheses.isValid2(str), false);
+    }
+
+    @Test
+    public void testParenthesisInvalidCharacter() {
+        String str = "()(){()}=";
+
+        Assert.assertEquals(lc_20_validParentheses.isValid2(str), false);
+    }
 }
