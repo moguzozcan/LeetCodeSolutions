cc568b20dc386756c4faaaed0211768d2d0c64ae
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-02 18:00:31 +0300

    LC 448 Updated

--- src/main/java/easy/LC_448_FindAllNumbersDissappearedInAnArray.java
+++ src/main/java/easy/LC_448_FindAllNumbersDissappearedInAnArray.java
@@ -19,13 +19,35 @@ Output:
  */
 public class LC_448_FindAllNumbersDissappearedInAnArray {
 
-    /*
-     The trick is that, loop through the array, for the elements, go to the location whose index is the one minus element,
-     then convert that element in that location to its negative one, if it is positive, meaning if it did not converted
-     before. Then, loop array again and find the ones that are positive, they are the ones that are not in the list,
-     but be careful that the index was one minus, so when finding the number add 1 to the indexes.
-     */
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        Arrays.sort(nums);
+
+        boolean[] nums2 = new boolean[nums.length];
+
+        List<Integer> disappearedElements = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            nums2[nums[i] - 1] = true;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums2[i] == false) {
+                disappearedElements.add(i + 1);
+            }
+        }
+
+        return disappearedElements;
+    }
+
+    /**
+     * The trick is that, loop through the array, for the elements, go to the location whose index is the one minus element,
+     * then convert that element in that location to its negative one, if it is positive, meaning if it did not converted
+     * before. Then, loop array again and find the ones that are positive, they are the ones that are not in the list,
+     * but be careful that the index was one minus, so when finding the number add 1 to the indexes.
+     * @param nums
+     * @return
+     */
+    public List<Integer> findDisappearedNumbersOn(int[] nums) {
         for(int i : nums) {
             if(nums[Math.abs(i) - 1] > 0) {
                 nums[Math.abs(i) - 1] = -1 * nums[Math.abs(i) - 1];
