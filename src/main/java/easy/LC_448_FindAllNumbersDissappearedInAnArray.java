62125033ae3a14408b4d900ca08315f2bd0274f5
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-02 18:05:27 +0300

    Updates companies info

--- src/main/java/easy/LC_448_FindAllNumbersDissappearedInAnArray.java
+++ src/main/java/easy/LC_448_FindAllNumbersDissappearedInAnArray.java
@@ -3,6 +3,9 @@ package easy;
 import java.util.*;
 
 /*
+Difficulty: Medium
+Companies: Google
+
 Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
 
 Find all the elements of [1, n] inclusive that do not appear in this array.
--- src/main/java/easy/LC_463_IslandPerimeter.java
+++ src/main/java/easy/LC_463_IslandPerimeter.java
@@ -1,6 +1,9 @@
 package easy;
 
 /*
+Difficulty: Easy
+Companies: Google
+
 You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
 
 Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
