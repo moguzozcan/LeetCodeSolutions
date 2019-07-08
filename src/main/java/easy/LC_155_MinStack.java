fafe04f2d0c1f6bd447fffed727c2027a45af5ff
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-08 23:06:28 +0300

    LC 155 Update

--- src/main/java/easy/LC_155_MinStack.java
+++ src/main/java/easy/LC_155_MinStack.java
@@ -1,11 +1,22 @@
+package easy;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /*
+Difficulty: Easy
+Companies: Google
+
 Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
 push(x) -- Push element x onto stack.
 pop() -- Removes the element on top of the stack.
 top() -- Get the top element.
 getMin() -- Retrieve the minimum element in the stack.
+
+
 Example:
+
 MinStack minStack = new MinStack();
 minStack.push(-2);
 minStack.push(0);
@@ -14,21 +25,14 @@ minStack.getMin();   --> Returns -3.
 minStack.pop();
 minStack.top();      --> Returns 0.
 minStack.getMin();   --> Returns -2.
- */
 
-package easy;
-
-import java.util.ArrayDeque;
-import java.util.Deque;
-
-/**
- * The idea in this question is to create a new data structure type, and store the current minimum of the all values
- * with them. Then return the min of the current value.
- */
+*/
 
 public class LC_155_MinStack {
 
     /**
+     * The idea in this question is to create a new data structure type, and store the current minimum
+     * of the all values with them. Then return the min of the current value.
      * initialize your data structure here.
      */
     private Deque<Element> stack;
