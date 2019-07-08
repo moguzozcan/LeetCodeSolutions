2882406c4e46b43d248f89f0a458f9e066ad98c4
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-08 17:37:44 +0300

    LC 67 Solution added

--- src/main/java/easy/LC_67_AddBinary.java
+++ src/main/java/easy/LC_67_AddBinary.java
@@ -1,6 +1,9 @@
 package easy;
 
 /*
+Difficulty: Easy
+Companies: Facebook
+
 Given two binary strings, return their sum (also a binary string).
 
 The input strings are both non-empty and contains only characters 1 or 0.
