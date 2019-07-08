19c8c43969ae03cc39151d58ffc6affd659e81c6
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-08 15:39:37 +0300

    Update lc 1

--- src/main/java/easy/LC_1_TwoSum.java
+++ src/main/java/easy/LC_1_TwoSum.java
@@ -1,4 +1,6 @@
-package easy;/*
+package easy;
+
+/*
 Difficulty: Easy
 Companies: Facebook
 
@@ -14,7 +16,6 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
  */
-
 
 import java.util.HashMap;
 import java.util.Map;
