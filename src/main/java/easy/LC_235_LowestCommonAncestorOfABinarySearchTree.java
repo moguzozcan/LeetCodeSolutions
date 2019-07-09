96ca4c91fd4b1997b47730b981d3733cc5e1244a
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-09 10:40:55 +0300

    LC 235 Updated

--- src/main/java/easy/LC_235_LowestCommonAncestorOfABinarySearchTree.java
+++ src/main/java/easy/LC_235_LowestCommonAncestorOfABinarySearchTree.java
@@ -85,9 +85,9 @@ public class LC_235_LowestCommonAncestorOfABinarySearchTree {
         boolean finished = false;
 
         while (!finished) { //while(root!= null)
-            if (p.val < root.val && q.val > root.val) {
+            if (p.val <= root.val && q.val >= root.val) {
                 finished = true;
-            } else if (p.val > root.val && q.val < root.val) {
+            } else if (p.val >= root.val && q.val <= root.val) {
                 finished = true;  //here we can return root and at the end of while loop we need to return null
                 // since there may not be a solution
             } else if (p.val > root.val && q.val > root.val) {
@@ -97,5 +97,36 @@ public class LC_235_LowestCommonAncestorOfABinarySearchTree {
             }
         }
         return root;
+    }
+
+    public TreeNode lowestCommonAncestorIterative2(TreeNode root, TreeNode p, TreeNode q) {
+
+        // Value of p
+        int pVal = p.val;
+
+        // Value of q;
+        int qVal = q.val;
+
+        // Start from the root node of the tree
+        TreeNode node = root;
+
+        // Traverse the tree
+        while (node != null) {
+
+            // Value of ancestor/parent node.
+            int parentVal = node.val;
+
+            if (pVal > parentVal && qVal > parentVal) {
+                // If both p and q are greater than parent
+                node = node.right;
+            } else if (pVal < parentVal && qVal < parentVal) {
+                // If both p and q are lesser than parent
+                node = node.left;
+            } else {
+                // We have found the split point, i.e. the LCA node.
+                return node;
+            }
+        }
+        return null;
     }
 }
