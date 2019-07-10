a5888e2072253eda227ff5b8785ed448ef192851
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-10 16:11:04 +0300

    LC 521 updated

--- src/main/java/easy/LC_521_LongestUncommonSubsequence.java
+++ src/main/java/easy/LC_521_LongestUncommonSubsequence.java
@@ -1,11 +1,16 @@
 package easy;
 
 /*
-Given a group of two strings, you need to find the longest uncommon subsequence of this group of two strings. The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this subsequence should not be any subsequence of the other strings.
+Given a group of two strings, you need to find the longest uncommon subsequence of this group of two strings.
+The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this
+subsequence should not be any subsequence of the other strings.
 
-A subsequence is a sequence that can be derived from one sequence by deleting some characters without changing the order of the remaining elements. Trivially, any string is a subsequence of itself and an empty string is a subsequence of any string.
+A subsequence is a sequence that can be derived from one sequence by deleting some characters without changing
+the order of the remaining elements. Trivially, any string is a subsequence of itself and an empty string is
+a subsequence of any string.
 
-The input will be two strings, and the output needs to be the length of the longest uncommon subsequence. If the longest uncommon subsequence doesn't exist, return -1.
+The input will be two strings, and the output needs to be the length of the longest uncommon subsequence. If
+the longest uncommon subsequence doesn't exist, return -1.
 
 Example 1:
 Input: "aba", "cdc"
@@ -25,7 +30,7 @@ public class LC_521_LongestUncommonSubsequence {
     Bad question, word game, no need to explain
      */
     public int findLUSlength(String a, String b) {
-        if(a.equals(b)) {
+        if(a.length() == b.length()) {
             if(allCharactersSame(a, b)) {
                 return -1;
             } else {
