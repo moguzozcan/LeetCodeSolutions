44b229d7dcd12de361a6daa82ccef4093f8b996d
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-07-10 15:21:45 +0300

    LC 447 Updated

--- src/main/java/easy/LC_447_NumberOfBumerangs.java
+++ src/main/java/easy/LC_447_NumberOfBumerangs.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /*
-Given n points in the plane that are all pairwise distinct, a "boomerang" is a tuple of points (i, j, k) such that the distance between i and j equals the distance between i and k (the order of the tuple matters).
+Given n points in the plane that are all pairwise distinct, a "boomerang" is a tuple of points (i, j, k) such that
+the distance between i and j equals the distance between i and k (the order of the tuple matters).
 
-Find the number of boomerangs. You may assume that n will be at most 500 and coordinates of points are all in the range [-10000, 10000] (inclusive).
+Find the number of boomerangs. You may assume that n will be at most 500 and coordinates of points are all in the
+range [-10000, 10000] (inclusive).
 
 Example:
 Input:
