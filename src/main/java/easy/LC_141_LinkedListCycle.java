69acdf31173df6c04a3e70319a217084fe1a400d
Muhammet Oğuz ÖZCAN <moguz00@gmail.com>
2019-10-01 10:56:58 +0300

    LC 141 Solution added

--- src/main/java/easy/LC_141_LinkedListCycle.java
+++ src/main/java/easy/LC_141_LinkedListCycle.java
@@ -76,6 +76,14 @@ public class LC_141_LinkedListCycle {
      * The idea is if we have two pointers and the fast one reached to end then there is no cycle,
      * If fast and slow meet at some point then this means that there is a cycle,
      * Time complexity is O(N)
+     * Both pointers are now in the cycle. Consider two runners running in a cycle - the fast runner moves 2 steps while the slow runner moves 1 steps at a time. Since the speed difference is 1, it takes \dfrac{\text{distance between the 2 runners}}{\text{difference of speed}}
+     * difference of speed
+     * distance between the 2 runners
+     * ​
+     *   loops for the fast runner to catch up with the slow runner. As the distance is at most "\text{cyclic length K}cyclic length K" and the speed difference is 1, we conclude that
+     * \text{Number of iterations} = \text{almost}Number of iterations=almost "\text{cyclic length K}cyclic length K".
+     *
+     * Therefore, the worst case time complexity is O(N+K)O(N+K), which is O(n)O(n).
      * Space complexity is O(1)
      * @param head
      * @return
